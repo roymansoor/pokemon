3ac3d95a0d7ebf2f251b2405057fe021664c79a1
roymansoor <roy.mansoor@gmail.com>
2022-12-26 16:11:06 -0600

    completed ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import Pokegame from "./Pokegame";
 
+import "./App.css";
 
 function App() {
-
-
   return (
     <div className="App">
-    
+      <Pokegame />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
